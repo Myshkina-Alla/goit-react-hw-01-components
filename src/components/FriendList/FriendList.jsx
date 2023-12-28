168834f0140e2855c5a06140621aacd08c535240
Myshkina-Alla <myshkina.alla@icloud.com>
2023-12-28 17:47:42 +0200

    Making edits

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,17 +1,9 @@
+import { FriendListItem } from './FriendListItem';
 import styles from '../FriendList/FriendList.module.css';
 const FriendList = ({ friends }) => (
   <ul className={styles.friendList}>
-    {friends.map(({ id, avatar, name, isOnline }) => (
-      <li key={id} className={styles.item}>
-        <span className={isOnline ? styles.online : styles.offline}></span>
-        <img
-          className={styles.avatar}
-          src={avatar}
-          alt="User avatar"
-          width="48"
-        />
-        <p className={styles.name}>{name}</p>
-      </li>
+    {friends.map(friend => (
+      <FriendListItem key={friend.id} {...friend} />
     ))}
   </ul>
 );
